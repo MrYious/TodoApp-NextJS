05467dc587289054dbd9aa66d67fcfc87815cf03
MrYious <rosariomark37@gmail.com>
2024-02-05 16:12:24 +0800

    refactor Task and style status bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,8 @@
 
 import { useEffect, useState } from "react";
 
-import CircleState from "./_components/CircleState";
 import Image from 'next/image';
-import check from "./_assets/icon-check.svg";
-import cross from "./_assets/icon-cross.svg";
+import Task from "./_components/Task";
 import moon from "./_assets/icon-moon.svg";
 import sun from "./_assets/icon-sun.svg";
 import { v4 as uuidv4 } from 'uuid';
@@ -42,7 +40,7 @@ export default function Home() {
       if (task.id === id) {
         return {
           ...task,
-          state: 'Completed'
+          state: task.state ==='Active'? 'Completed' : 'Active'
         };
       }
       return task;
@@ -56,8 +54,6 @@ export default function Home() {
 
   const handleKeyPress = (e: any) => {
     if (e.key === 'Enter') {
-      // Call your function or handle logic here
-      console.log('Enter key pressed!');
       if (text !== '') {
         setTasks([
           ...tasks,
@@ -74,7 +70,7 @@ export default function Home() {
 
   return (
     <main className="bg-bg-desktop-light min-h-screen bg-no-repeat bg-white flex justify-center ">
-      <div className="flex flex-col border-2 border-black w-1/2 p-20">
+      <div className="flex flex-col w-1/2 p-20">
         {/* HEADER */}
         <div className="flex items-center justify-between w-full">
           <div className=" tracking-[0.4em] text-4xl text-white font-bold">
@@ -108,32 +104,27 @@ export default function Home() {
           {/* List */}
           {
             tasks.filter((task) => task.state === filter || filter === "All" ).map((task, i) =>
-              <div className="flex py-4 px-5 items-center gap-5 border-b-[1px] border-gray-200 ">
-                <CircleState key={task.id} state={task.state} />
-                <div className="w-full">
-                  {task.text}
-                </div>
-              </div>
+              <Task key={task.id} task={task} toggleState={toggleState} />
             )
           }
           {/* Status Bar */}
           <div className="flex justify-between items-center rounded-b-md text-xs font-bold py-3 px-5 ">
-            <div>
-              {tasks.length} items left
+            <div className="text-gray-500">
+              {tasks.filter((task)=> task.state === 'Active').length} items left
             </div>
             <div className="flex gap-3">
-              <button onClick={()=>{setFilter('All')}}>
+              <button className={` hover:text-black ${filter === 'All' ? 'text-blue-800' : 'text-gray-500' }`} onClick={()=>{setFilter('All')}}>
                 All
               </button>
-              <button onClick={()=>{setFilter('Active')}}>
+              <button className={` hover:text-black ${filter === 'Active' ? 'text-blue-800' : 'text-gray-500' }`} onClick={()=>{setFilter('Active')}}>
                 Active
-                </button>
-              <button onClick={()=>{setFilter('Completed')}}>
+              </button>
+              <button className={` hover:text-black ${filter === 'Completed' ? 'text-blue-800' : 'text-gray-500' }`} onClick={()=>{setFilter('Completed')}}>
                 Completed
               </button>
             </div>
             <div>
-              <button onClick={handleClearCompleted}>Clear Completed</button>
+              <button className="text-gray-500 hover:text-black" onClick={handleClearCompleted}>Clear Completed</button>
             </div>
           </div>
         </div>
